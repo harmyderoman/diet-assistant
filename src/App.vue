200e67ff96788d250332341e283418cb78b195bb
harmyderoman <romangarmider@gmail.com>
2019-07-15 19:41:07 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Diet Assistant</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      
+      
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn
+              flat
+                color="primary"
+                dark
+                v-on="on"
+              >
+                User
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-tile
+                v-for="(ulink, index) in userlinks"
+                :key="index"
+                @click=""
+              >
+                <v-list-tile-title>{{ ulink.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+     
+      
+    </v-toolbar-items>
+  </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      userlinks: [
+        { index: 1, title: "User Info"},
+        { index: 2, title: "Diet Plan"},
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
